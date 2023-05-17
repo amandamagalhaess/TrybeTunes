7a27f27973b091b126dfb1b3319063709d90661a
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-05-16 21:30:41 -0300

    5. Crie o formulário para pesquisar artistas

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,34 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    isSearchButtonDisabled: true,
+  };
+
+  handleInput = ({ target }) => {
+    this.setState(() => (target.value.length >= 2
+      ? { isSearchButtonDisabled: false }
+      : { isSearchButtonDisabled: true }));
+  };
+
   render() {
+    const { isSearchButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <div>
+          <input
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            onChange={ this.handleInput }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ isSearchButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
